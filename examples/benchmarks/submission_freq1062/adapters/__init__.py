b1c682e7b6951d2aa7adf7a333404019549f9f5b
Alex Min <frequentiststatistics@gmail.com>
2026-06-27 17:24:02 -0400

    Fix __init__.py mangled by import sorter

--- examples/benchmarks/submission_freq1062/adapters/__init__.py
+++ examples/benchmarks/submission_freq1062/adapters/__init__.py
@@ -7,4 +7,13 @@ from .memanto import MemantoAdapter
 from .supermemory import SupermemoryAdapter
 from .vector_baseline import VectorBaselineAdapter
 from .zep_graphiti import ZepAdapter
+
+__all__ = [
+    "CogneeAdapter",
+    "LettaAdapter",
+    "Mem0Adapter",
+    "MemantoAdapter",
+    "SupermemoryAdapter",
+    "VectorBaselineAdapter",
+    "ZepAdapter",
 ]
